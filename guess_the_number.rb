b22dd8b4bc8182d5c6376794b035e028c715f9f1
krez274 <keeganperez@icloud.com>
2014-03-18 18:06:49 +0000

    we made a game that guesses a number

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,5 +1,20 @@
 puts "Welcome to guess the number."
-
 secret_number = rand(689)
-
 puts secret_number
+puts "Guess a number between (0 - 689)"
+guess = gets.chomp.to_i
+if secret_number = guess
+puts "congrats you guessed the number right"
+else 
+puts "FAIL#"69""
+end
+
+
+
+
+
+
+
+
+
+
